759331ca9e6257f820d5b9c45bcce50726586446
Samay Sah <70233988+SamaySah21@users.noreply.github.com>
2022-03-10 20:01:30 +0530

    Line directly Added in git

--- practice.py
+++ practice.py
@@ -1,3 +1,5 @@
-print("Hello Git")
+print("Owner : Hello Git")
 
-print("Hi i am adding one more line code")
+print("Contributor : Hi i am adding one more line code")
+
+print("Owner : Hi i am adding one more line directly in git source code ... please all contributor take pull request")
