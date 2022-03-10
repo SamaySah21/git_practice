9aeb4553387861c46d0649dc60ea9e6271d919b9
Samay Sah <samaysah21@gmail.com>
2022-03-10 21:39:16 +0530

    main first change

--- practice.py
+++ practice.py
@@ -3,3 +3,7 @@ print("Owner : Hello Git")
 print("Contributor : Hi i am adding one more line code")
 
 print("Owner : Hi i am adding one more line directly in git source code ... please all contributor take pull request")
+'''
+THis is main branch first change
+
+'''
