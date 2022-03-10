2079b82860a0d9f7de4bd1f8fe60813b0ec5805c
Samay Sah <samaysah21@gmail.com>
2022-03-10 21:40:59 +0530

    branch 1 second change

--- practice.py
+++ practice.py
@@ -5,5 +5,6 @@ print("Contributor : Hi i am adding one more line code")
 print("Owner : Hi i am adding one more line directly in git source code ... please all contributor take pull request")
 '''
 THis is main branch1 first change
-
+THis is main branch1  change
+THis is main branch1 second change
 '''
